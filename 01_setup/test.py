341332fbdceb6ca89daf1331a1a3aa8daf40e5c6
husseinrabie <hussien.rabi@gamil.com>
2025-06-29 11:25:17 +0300

    typecast file added

--- 01_setup/test.py
+++ 01_setup/test.py
@@ -2,3 +2,7 @@ print("Hello Python")
 print("Hello Python")
 print("Hello Python")
 print("Hello Python")
+
+text = input("Enter some text: ")
+upper_text = text.upper()
+print(f"You entered: {upper_text}")
---
+++ 04_typecasting/typecast.py
@@ -0,0 +1,11 @@
+a= 33
+b ="33"
+print(a) 
+print(type(a))
+print(b)
+print(type(b))
+
+# Typecasting
+c= int(b)  # Convert string to integer
+print(c)
+print(type(c))
