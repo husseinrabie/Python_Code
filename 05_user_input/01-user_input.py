df6b7041734b004e0678263e75641f41cd5a90ff
husseinrabie <hussien.rabi@gamil.com>
2025-06-29 12:19:11 +0300

    File Ended

---
+++ 05_user_input/01-user_input.py
@@ -0,0 +1,11 @@
+age=input("please enter a number: ")
+
+name = input("please enter your name: ")
+
+print (f"Hello {name } you are age is {age} years old")
+
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+print(f"The sum of {a} and {b} is {a + b}")
