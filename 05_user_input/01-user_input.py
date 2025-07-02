16b812e35584526d876760d084c81152a9f3ee9e
husseinrabie <hussien.rabi@gamil.com>
2025-07-02 08:04:27 +0300

    escape charchter added

--- 05_user_input/01-user_input.py
+++ 05_user_input/01-user_input.py
@@ -10,3 +10,10 @@ b = int(input("Enter another number: "))
 
 print(f"The sum of {a} and {b} is {a + b}")
 print(f"The difference of {a} and {b} is {a - b}")
+
+# single line comment
+
+"""This is a multi-line comment
+You can write multiple lines here  
+and it will be ignored by the interpreter
+"""
